246a8691db7ee40e1ee57c549f8ca7621ca0e187
Ananya <justdiscussanything@gmail.com>
2025-07-22 17:18:18 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home";
-import Nav from "./components/Nav.jsx";
-
-
-
-
+import Nav from "./components/Nav";
 const App = ()=>{
-
-
 
   return <>
   
